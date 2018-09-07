d697f9b5f7b156ef4676a15c5d6901075b6370ee
Ben Linsay <blinsay@gmail.com>
2018-09-07 18:29:57 -0400

    use x/net/dns/dnsmessage instead of miekg/dns

--- main.go
+++ main.go
@@ -8,207 +8,237 @@ import (
 	"io"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
 	"strings"
 
-	"github.com/miekg/dns"
+	"golang.org/x/net/dns/dnsmessage"
 )
 
 var (
-	// args.
-	endpoint string
-	name     string
+	// dns-over-https options
+	post     = flag.Bool("post", false, "use a POST request to make a query. slightly smaller, but less cache friendly")
+	resolver = flag.String("resolver", "", "the url of the dns-over-https resolver to use")
 
-	// flags
-
-	// use GET
-	useGET = flag.Bool("get", false, "use a GET request to make a query instead of a POST request")
-	// print raw http requests and responses
-	dumpHTTP = flag.Bool("dump-http", false, "dumps http request/response headers")
-	// the query type. parsed as a string, and then as an int if unrecognized.
-	qtypeArg = flag.String("type", "A", "the `type` of record to query for. defaults to A")
-	// the query class. defaults to IN, will never change?
+	// query options
+	qtypeArg  = flag.String("type", "A", "the `type` of record to query for. defaults to A")
 	qclassArg = flag.String("class", "IN", "the `class` of record to query for. defaults to IN")
+
+	// output options
+	short    = flag.Bool("short", false, "provide a terse answer, like dig's +short")
+	dumpHTTP = flag.Bool("dump-http", false, "dumps http request/response headers")
+)
+
+var (
+	stringToClass = map[string]dnsmessage.Class{
+		"IN":  dnsmessage.ClassINET,
+		"CS":  dnsmessage.ClassCSNET,
+		"CH":  dnsmessage.ClassCHAOS,
+		"HS":  dnsmessage.ClassHESIOD,
+		"ANY": dnsmessage.ClassANY,
+	}
+
+	stringToType = map[string]dnsmessage.Type{
+		"A":     dnsmessage.TypeA,
+		"NS":    dnsmessage.TypeNS,
+		"CNAME": dnsmessage.TypeCNAME,
+		"SOA":   dnsmessage.TypeSOA,
+		"PTR":   dnsmessage.TypePTR,
+		"MX":    dnsmessage.TypeMX,
+		"TXT":   dnsmessage.TypeTXT,
+		"AAAA":  dnsmessage.TypeAAAA,
+		"SRV":   dnsmessage.TypeSRV,
+		"OPT":   dnsmessage.TypeOPT,
+	}
 )
 
 func init() {
 	log.SetFlags(0)
 
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [query flags] [endpoint] [names...]\n\n", os.Args[0])
-		fmt.Fprintf(flag.CommandLine.Output(), "%s makes a dns-over-https query to the given endpoint.\n\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "usage: %s [query flags] -resolver [url] [names...]\n\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "%s makes a dns-over-https query.\n\n", os.Args[0])
 		fmt.Fprintf(flag.CommandLine.Output(), "available options:\n")
 		flag.PrintDefaults()
 	}
-	flag.Parse()
 }
 
 func main() {
-	args := flag.Args()
-	if len(args) < 2 {
-		log.Fatalln("endpoint and a query name are both required")
+	flag.Parse()
+
+	if *resolver == "" {
+		log.Fatalf("--resolver is required")
 	}
 
-	qclass, ok := qclassToInt(*qclassArg)
+	qclass, ok := stringToClass[strings.ToUpper(*qclassArg)]
 	if !ok {
+		// FIXME: print supported classes
 		log.Fatalf("unrecognized query class: %s", *qclassArg)
 	}
-	qtype, ok := qtypeToSInt(*qtypeArg)
+	qtype, ok := stringToType[strings.ToUpper(*qtypeArg)]
 	if !ok {
+		// FIXME: print supported types
 		log.Fatalf("unrecognized query type: %s", *qtypeArg)
 	}
 
+	// TODO(benl): customize the bootstrap resolver for the dns-over-https server
+	// TODO(benl): force http2?
 	client := http.Client{}
 
-	endpoint := args[0]
-	for _, name := range args[1:] {
-		// build the request and dump it before sending.
-		var request *http.Request
-		var err error
-		if *useGET {
-			request, err = NewGetRequest(endpoint, dns.Fqdn(name), qclass, qtype, true)
-		} else {
-			request, err = NewPostRequest(endpoint, dns.Fqdn(name), qclass, qtype, true)
-		}
-		if err != nil {
-			log.Fatalln("error building request:", err)
-		}
-		request.Header.Set("User-Agent", "github.com/blinsay/homer")
-
-		if *dumpHTTP {
-			requestBs, err := httputil.DumpRequestOut(request, false)
-			if err != nil {
-				panic(err)
-			}
-			log.Println(string(requestBs))
-
-			bodyBs, err := copyRequestBody(request)
-			if err != nil {
-				panic(err)
-			}
-			if len(bodyBs) > 0 {
-				log.Println(hex.Dump(bodyBs))
-			}
-		}
-
-		response, err := client.Do(request)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// dump the response http request and body separately so that the response
-		// can get hexdumped. this isn't toally necessary but makes it easier to
-		// inspect the dns wire format.
-		if *dumpHTTP {
-			bs, err := httputil.DumpResponse(response, false)
-			if err != nil {
-				panic(err)
-			}
-			log.Println(string(bs))
-
-			bodyBs, err := copyResponseBody(response)
-			if err != nil {
-				panic(err)
-			}
-			if len(bodyBs) > 0 {
-				log.Println(hex.Dump(bodyBs))
-			}
-		}
-
-		// parse the dns response if and only if we got a 200
-		if response.StatusCode != 200 {
-			log.Fatalln("error: unexpcted response code:", response.StatusCode)
-		}
-		dnsResponse, err := ReadDNSResponse(response)
-		if err != nil {
-			log.Fatal("error:", err)
-		}
-		fmt.Println(dnsResponse)
-	}
-}
-
-func qclassToInt(qclass string) (uint16, bool) {
-	qclass = strings.ToUpper(qclass)
-
-	for id, name := range dns.ClassToString {
-		if qclass == name {
-			return id, true
-		}
+	// per the RFC, application/dns-message requests should use a message id of
+	// zero for cache friendliness.
+	//
+	// TODO(benl): a cli flag to disable recursive queries
+	question := dnsmessage.Message{
+		Header: dnsmessage.Header{
+			ID:               0,
+			RecursionDesired: true,
+		},
 	}
 
-	// TODO(benl): try parsing qclass as an int and returning that
-
-	return 0, false
-}
-
-func qtypeToSInt(qtype string) (uint16, bool) {
-	qtype = strings.ToUpper(qtype)
-
-	for id, name := range dns.TypeToString {
-		if qtype == name {
-			return id, true
+	for _, name := range flag.Args() {
+		canonicalName := name
+		if !strings.HasSuffix(canonicalName, ".") {
+			canonicalName = canonicalName + "."
 		}
+		question.Questions = append(question.Questions, dnsmessage.Question{
+			Class: qclass,
+			Type:  qtype,
+			Name:  dnsmessage.MustNewName(canonicalName),
+		})
 	}
 
-	// TODO(benl): try parsing qclass as an int and returning that
-
-	return 0, false
-}
-
-func appendToFile(filename string, bs []byte) error {
-	f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	var err error
+	var request *http.Request
+	if *post {
+		request, err = postRequest(*resolver, question)
+	} else {
+		request, err = getRequest(*resolver, question)
+	}
 	if err != nil {
-		return err
+		log.Fatalln("error building request:", err)
+	}
+	request.Header.Set("User-Agent", "github.com/blinsay/homer")
+
+	if *dumpHTTP {
+		reqDump, err := dumpRequest(request)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(reqDump)
 	}
 
-	n, err := f.Write(bs)
-	if err == nil && n < len(bs) {
-		err = io.ErrShortWrite
+	response, err := client.Do(request)
+	if err != nil {
+		log.Fatal(err)
 	}
-	if closeErr := f.Close(); err == nil {
-		err = closeErr
+
+	if *dumpHTTP {
+		respDump, err := dumpResponse(response)
+		if err != nil {
+			panic(err)
+		}
+		log.Println(respDump)
 	}
-	return err
+
+	if response.StatusCode == 200 {
+		msg, err := dnsResponse(response)
+		if err != nil {
+			log.Fatal("error parsing dns-over-http response:", err)
+		}
+
+		for _, answer := range msg.Answers {
+			if *short {
+				log.Println(formatBody(answer.Body))
+			} else {
+				log.Println(formatHeader(answer.Header), formatBody(answer.Body))
+			}
+		}
+	}
 }
 
-// try to copy a request body and replace it with a bytes.Buffer
-func copyRequestBody(r *http.Request) ([]byte, error) {
-	if r.Body == http.NoBody || r.Body == nil {
-		return nil, nil
+func formatHeader(h dnsmessage.ResourceHeader) string {
+	typ := h.Type.String()
+	if strings.HasPrefix(typ, "Type") {
+		typ = typ[4:]
 	}
-
-	oldb := r.Body
-	defer oldb.Close()
-
-	bs, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	r.Body = ioutil.NopCloser(bytes.NewBuffer(bs))
-	r.ContentLength = int64(len(bs))
-	r.GetBody = func() (io.ReadCloser, error) {
-		return ioutil.NopCloser(bytes.NewReader(bs)), nil
-	}
-	return bs, nil
+	return fmt.Sprintf("%s %d %s", h.Name, h.TTL, typ)
 }
 
-// try to copy a response body and replace it with a bytes.Buffer
-func copyResponseBody(r *http.Response) ([]byte, error) {
-	if r.Body == http.NoBody || r.Body == nil {
-		return nil, nil
+func formatBody(b dnsmessage.ResourceBody) string {
+	switch rr := b.(type) {
+	case *dnsmessage.AResource:
+		return net.IP(rr.A[:]).String()
+	case *dnsmessage.NSResource:
+		return rr.NS.String()
+	case *dnsmessage.CNAMEResource:
+		return rr.CNAME.String()
+	case *dnsmessage.SOAResource:
+		return fmt.Sprintf("%s %s %d %d %d %d %d", rr.NS, rr.MBox, rr.Serial, rr.Refresh, rr.Retry, rr.Expire, rr.MinTTL)
+	case *dnsmessage.PTRResource:
+		return rr.PTR.String()
+	case *dnsmessage.MXResource:
+		return fmt.Sprintf("%d %s", rr.Pref, rr.MX)
+	case *dnsmessage.TXTResource:
+		return fmt.Sprintf("%q", strings.Join(rr.TXT, " "))
+	case *dnsmessage.AAAAResource:
+		return net.IP(rr.AAAA[:]).String()
 	}
 
-	oldb := r.Body
-	defer oldb.Close()
+	return "(unknown)"
+}
 
-	bs, err := ioutil.ReadAll(r.Body)
+func dumpRequest(request *http.Request) (string, error) {
+	requestBytes, err := httputil.DumpRequestOut(request, false)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	r.Body = ioutil.NopCloser(bytes.NewBuffer(bs))
+	headers := string(requestBytes)
 
-	return bs, nil
+	if request.Body == http.NoBody || request.Body == nil {
+		return headers, nil
+	}
+
+	oldBody := request.Body
+	defer oldBody.Close()
+
+	bodyBytes, err := ioutil.ReadAll(request.Body)
+	if err != nil {
+		return "", err
+	}
+
+	request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	request.ContentLength = int64(len(bodyBytes))
+	request.GetBody = func() (io.ReadCloser, error) {
+		return ioutil.NopCloser(bytes.NewReader(bodyBytes)), nil
+	}
+
+	if len(bodyBytes) > 0 {
+		return strings.Join([]string{headers, hex.Dump(bodyBytes)}, "\n"), nil
+	}
+	return headers, nil
+}
+
+func dumpResponse(response *http.Response) (string, error) {
+	bs, err := httputil.DumpResponse(response, false)
+	if err != nil {
+		return "", err
+	}
+	headers := string(bs)
+
+	oldBody := response.Body
+	defer oldBody.Close()
+
+	bodyBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return "", err
+	}
+	response.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+
+	if len(bodyBytes) > 0 {
+		return strings.Join([]string{headers, hex.Dump(bodyBytes)}, "\n"), nil
+	}
+	return headers, nil
 }
